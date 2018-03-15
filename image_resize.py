ad3fc9fa181b1c7b115e68242958ff42c255cb94
KangLLL <likang@oregonstate.edu>
2018-03-14 23:15:02 -0700

    a

--- image_resize.py
+++ image_resize.py
@@ -4,15 +4,17 @@ import shutil
 from PIL import Image
 from resizeimage import resizeimage
 
-def resize_file(csv_file, target_dir, target_size):
+def resize_file(csv_file, source_dir, target_dir, target_size):
     with open('./train.csv') as f:
         lines = f.read().splitlines()
         del lines[0]
         for i in range(len(lines)):
             row = lines[i].split(',')
-            file_path = os.path.join(target_dir, row[0])
 
-            with open(row[0], 'r+b') as f:
+            file_path = os.path.join(target_dir, row[0])
+            source_path = os.path.join(source_dir, row[0])
+
+            with open(file_path, 'r+b') as f:
                 with Image.open(f) as image:
                     cover = resizeimage.resize_width(image, target_size)
                     cover.save(file_path, image.format)
@@ -26,7 +28,8 @@ if len(sys.argv) == 2:
 
 target = 'images_' + str(size)
 
-parent_dir = os.path.dirname(os.path.normpath(source))
+source_dir = os.path.normpath(source)
+parent_dir = os.path.dirname(source_dir)
 target_dir = os.path.join(parent_dir, target)
 
 if os.path.exists(target_dir):
@@ -34,7 +37,7 @@ if os.path.exists(target_dir):
 
 os.makedirs(target_dir)
 
-resize_file('./train.csv', target_dir, size)
-resize_file('./test.csv', target_dir, size)
+resize_file('./train.csv', source_dir, target_dir, size)
+resize_file('./test.csv', source_dir, target_dir, size)
 
 
